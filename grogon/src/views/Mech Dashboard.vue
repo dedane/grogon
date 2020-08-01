23fff0e132fc8cc76b79a367c7227e2d75efdef5
dedane <evanskeema@gmail.com>
2020-08-01 12:53:51 +0300

    rating

--- grogon/src/views/Mech Dashboard.vue
+++ grogon/src/views/Mech Dashboard.vue
@@ -3,10 +3,34 @@
     <v-container>
       <v-row>
         <v-col>
+          <v-card>
+            <v-card-title>
+              Cars Fixed
+            </v-card-title>
+            <v-card-subtitle>
+              132
+            </v-card-subtitle>
+          </v-card>
         </v-col>
         <v-col>
+          <v-card>
+            <v-card-title>
+              Average Transactions
+            </v-card-title>
+            <v-card-subtitle green>
+              ksh 142,000
+            </v-card-subtitle>
+          </v-card>
         </v-col>
         <v-col>
+          <v-card>
+            <v-card-title>
+              Average Transactions
+            </v-card-title>
+            <v-card-actions>
+              <v-rating v-model="rating"></v-rating>
+            </v-card-actions>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -27,6 +51,9 @@ export default {
   components: {
     Maps,
   },
+  data : () => ({
+    rating : 4,
+  }),
 };
 </script>
 
